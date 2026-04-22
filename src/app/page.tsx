b696db4ebd1e0cfd9c0398f7eaf9c22e6d3105f4
Leonardo Klaser <leonardo.klaser@sap.com>
2026-04-21 23:20:49 -0300

    feat: restructure page with new section order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,25 @@
 import Hero from "@/components/Hero";
+import ServicesGrid from "@/components/ServicesGrid";
+import ServiceCarousel from "@/components/ServiceCarousel";
+import ContentPillars from "@/components/ContentPillars";
 import AboutMe from "@/components/AboutMe";
 import PainPoints from "@/components/PainPoints";
 import Marquee from "@/components/Marquee";
-import ValueLadder from "@/components/ValueLadder";
-import ProductSection from "@/components/ProductSection";
-import DirecioneSection from "@/components/DirecioneSection";
 import CTASection from "@/components/CTASection";
 import Footer from "@/components/Footer";
-import { SERVICES } from "@/lib/constants";
+import { CAROUSEL_SERVICES } from "@/lib/constants";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <ServicesGrid />
+      {CAROUSEL_SERVICES.map((service) => (
+        <ServiceCarousel key={service.id} service={service} />
+      ))}
+      <ContentPillars />
       <AboutMe />
       <PainPoints />
-      <Marquee />
-      <ValueLadder />
-      {SERVICES.map((service, index) => (
-        <ProductSection key={service.step} service={service} index={index} />
-      ))}
-      <DirecioneSection />
       <Marquee />
       <CTASection />
       <Footer />
